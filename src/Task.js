5ae40c32db9411d8995acaad02e7ae87aea0e85b
Oleg <oleg.pav.m@gmail.com>
2019-08-05 11:40:32 +0300

    Task.

--- src/Task.js
+++ src/Task.js
@@ -1,14 +1,60 @@
+const sleep = require('then-sleep');
+
 class Task {
-    constructor(params, bitmex) {
-        // TODO -
+    constructor(bitmex, { price, value, step5 }) {
+        this._startDate = new Date();
+        this._currentHours = this._startDate.getHours();
+        this._price = price;
+        this._value = value;
+        this._step = step5 / 5;
+        this._bitmex = bitmex;
+        this._orderID = null;
+        this._currentPrice = this._price;
+
+        setImmediate(this._start.bind(this));
+    }
+
+    async _start() {
+        await this._initOrder();
+
+        while (true) {
+            const hours = new Date().getHours();
+
+            if (this._currentHours !== hours) {
+                this._currentHours = hours;
+                await this._iteration();
+            }
+            sleep(5000);
+        }
+    }
+
+    async _initOrder() {
+        const result = await this._bitmex.placeOrder(this._price, this._value);
+
+        this._orderID = result.orderID;
+    }
+
+    async _iteration() {
+        const hoursDiff = (new Date() - this._startDate) / 1000 / 60 / 60;
+        const price = Math.round(this._price + this._step * hoursDiff);
+
+        this._currentPrice = price;
+
+        await this._bitmex.moveOrder(this._orderID, price, this._value);
     }
 
     async cancel() {
-        // TODO -
+        await this._bitmex.cancelOrder(this._orderID);
     }
 
     explain() {
-        // TODO -
+        return {
+            startDate: this._startDate,
+            initPrice: this._price,
+            currentPrice: this._currentPrice,
+            nextPrice: this._currentPrice + this._step,
+            step: this._step,
+        };
     }
 }
 
